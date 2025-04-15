26a9a583c63ad7d83b18b8eda3c96ee07a6b0c4d
Tobias Manago <110705712+tobias-manago@users.noreply.github.com>
2025-04-15 13:58:16 -0300

    termine arreglos paralelos

--- TP5 paralel arrays/arrays paralel/main.c
+++ TP5 paralel arrays/arrays paralel/main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int cargaArreglosParalelos(int matricula[20], char nombre[20][30]);
 void muestraArreglosParalelos(int matricula[20], char nombre[20][30], int validos);
-
+void mostrarNombrePorLegajo(int matricula[20], char nombre[20][30], int validos, int valorBuscado);
 int main()
 {
     int matricula [20];
@@ -13,12 +14,10 @@ int main()
     int valorBuscado;
     printf("que legajo desea encontrar ? \n");
     scanf("%d",&valorBuscado);
-    int pos = buscarLegajo(matricula, validos,valorBuscado);
-if (pos != -1) {
-    printf("Legajo encontrado en la posicion: %d \n", pos);
-} else {
-    printf("Legajo no encontrado.\n");
-}
+    mostrarNombrePorLegajo(matricula,nombre,validos,valorBuscado);
+    ordenarPorNombre(matricula, nombre, validos);
+    muestraArreglosParalelos(matricula, nombre, validos);
+
 
 
     return 0;
@@ -57,16 +56,57 @@ void muestraArreglosParalelos(int matricula[20], char nombre[20][30], int valido
         i++;
     }
 }
-int buscarLegajo(int matricula[20], int validos, int valorBuscado) {
+int buscarLegajo(int matricula[20], int validos, int valorBuscado)
+{
     int i = 0;
 
-    while (i < validos) {
-        if (matricula[i] == valorBuscado) {
-            return i; // Retorna la posición donde se encontró el legajo
+    while (i < validos)
+    {
+        if (matricula[i] == valorBuscado)
+        {
+            return i;
         }
         i++;
     }
 
-    return -1; // Si no se encuentra, retorna -1
+    return -1;
+}
+void mostrarNombrePorLegajo(int matricula[20], char nombre[20][30], int validos, int valorBuscado)
+{
+    int posicion = buscarLegajo(matricula,validos,valorBuscado);
+
+    if (posicion != -1)
+    {
+        printf("El nombre correspondiente al legajo %d es: ", valorBuscado);
+        puts(nombre[posicion]);
+    }
+    else
+    {
+        printf("Legajo %d no encontrado.\n", valorBuscado);
+    }
+}
+void ordenarPorNombre(int matricula[20], char nombre[20][30], int validos) {
+    int i, j, min;
+    char auxNombre[30];
+    int auxMatricula;
+
+    for (i = 0; i < validos - 1; i++) {
+        min = i;
+        for (j = i + 1; j < validos; j++) {
+            if (strcmp(nombre[j], nombre[min]) < 0) {
+                min = j;
+            }
+        }
+
+        // Intercambiar nombres
+        strcpy(auxNombre, nombre[i]);
+        strcpy(nombre[i], nombre[min]);
+        strcpy(nombre[min], auxNombre);
+
+        // Intercambiar matrículas
+        auxMatricula = matricula[i];
+        matricula[i] = matricula[min];
+        matricula[min] = auxMatricula;
+    }
 }
 
